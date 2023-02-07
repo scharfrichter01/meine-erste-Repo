aa7437d758654d3e315ae375221a11b74982e309
scharfrichter <scharfrichter1889@gmail.com>
2023-02-07 23:53:13 +0300

    Add New comment
    yes, a new comment

--- project.py
+++ project.py
@@ -2,26 +2,42 @@
 from module import *
 
 global data, columns
-# columns, data = read_from_file('data.csv')
-# print(data)
-# insert(get_line())
-# print_data()
-# write_to_file('data.csv')
 
-# columns = ('id', 'name', 'lastname', 'age', 'height', 'weight')
-# data = [
-#     (1, 'Ivan', 'Ivanov', 14, 160, 50),
-#     (2, 'Sasha', 'Sidorov', 15, 210, 40),
-#     (3, 'Masha', 'Poradina', 30, 178, 70),
-#     (4, 'Timur', 'Korolev', 44, 160, 120)
-# ]
+"""
+
+    Нужные нам формулы
+
+    # columns, data = read_from_file('data.csv')
+    # print(data)
+    # insert(get_line())
+    # print_data()
+    # write_to_file('data.csv')
+    # ChangeLine() 
+
+"""
+
+"""
+
+    Это нам уже не нужно, выводить из под комментария только в случае обновления баззы данных
+
+    # columns = ('id', 'name', 'lastname', 'age', 'height', 'weight')
+    # data = [
+    #     (1, 'Ivan', 'Ivanov', 14, 160, 50),
+    #     (2, 'Sasha', 'Sidorov', 15, 210, 40),
+    #     (3, 'Masha', 'Poradina', 30, 178, 70),
+    #     (4, 'Timur', 'Korolev', 44, 160, 120)
+    # ]
+
+"""
+
 print_data()
 
 columns, data = read_from_file('data.csv')
 
 # ChangeLine()
-# insert(get_line())
 
-# write_to_file('data.csv')
+insert(get_line())
 
-# print_data()
+write_to_file('data.csv')
+
+print_data()
